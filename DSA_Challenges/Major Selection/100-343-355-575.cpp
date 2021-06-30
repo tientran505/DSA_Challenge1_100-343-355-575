352cdc91100e0271a7cbb4328ddd143fad29b37d
HuynhNguyen0811 <huynhnguyen08112002@gmail.com>
2021-06-30 23:18:01 +0700

    read grading

--- DSA_Challenges/Major Selection/100-343-355-575.cpp
+++ DSA_Challenges/Major Selection/100-343-355-575.cpp
@@ -1,6 +1,89 @@
-#include <iostream>
-#include <string>
-#include <fstream>
+#include "struct.h"
+
+void wstringToString(std::wstring source, std::string& destination) {
+	int length = source.size();
+	for (int i = 0; i < length; i++) {
+		destination[i] = source[i];
+	}
+}
+
+int isFoundation(std::string str) { //0 foundation, 1 sub, 2 general
+	std::string temp;
+
+	temp = str;
+	temp.erase(temp.begin() + 4, temp.end());
+	if (temp == "CSC1") return 0;
+
+	temp = str;
+	temp.erase(temp.begin() + 3, temp.end());
+	if (temp == "BAA") {
+		if (str[str.length() - 2] == '1' || str[str.length() - 2] == '2' || str[str.length() - 2] == '3') return 1;
+	}
+	return 2;
+}
+
+void readGrading(std::string fileName, StudentList stuList) {
+	std::ifstream fileIn;
+	fileIn.open(fileName, std::ios_base::in);
+
+	if (!fileIn.is_open()) {
+		std::cout << "Can't open\n";
+		return;
+	}
+
+	std::string temp;
+
+	while (!fileIn.eof()) {
+		getline(fileIn, temp, ','); //skip first col
+		getline(fileIn, temp, ',');
+
+		if (stuList.pTail == nullptr || stuList.pTail->data.studentID != temp) { //create new node
+			if (stuList.pHead == nullptr) {
+				stuList.pHead = new _Student;
+				stuList.pTail = stuList.pHead;
+			}
+			else {
+				stuList.pTail->data.gpa.GPA_All /= (stuList.pTail->data.gpa.credits_General + stuList.pTail->data.gpa.credits_Foundation);
+				stuList.pTail->data.gpa.GPA_Foundation /= stuList.pTail->data.gpa.credits_Foundation;
+				stuList.pTail->pNextStu = new _Student;
+				stuList.pTail = stuList.pTail->pNextStu;
+			}
+			stuList.pTail->data.studentID = temp;
+			getline(fileIn, stuList.pTail->data.firstName, ',');
+			getline(fileIn, stuList.pTail->data.lastName, ',');
+
+			for (int i = 0; i < 3; i++) getline(fileIn, temp, ',');
+		}
+		else { //add grade to existed node
+			for (int i = 0; i < 5; i++) getline(fileIn, temp, ',');
+		}
+
+		getline(fileIn, temp, ',');
+		int check = isFoundation(temp);
+
+		for (int i = 0; i < 2; i++) getline(fileIn, temp, ',');
+
+		getline(fileIn, temp, ',');
+		float gpa = stof(temp);
+		getline(fileIn, temp, ',');
+		getline(fileIn, temp, '\n');
+		int credit = stoi(temp);
+
+		if (check == 0) {
+			stuList.pTail->data.gpa.GPA_All += gpa * credit;
+			stuList.pTail->data.gpa.GPA_Foundation += gpa * credit;
+			stuList.pTail->data.gpa.credits_Foundation += credit;
+		}
+		if (check == 1) {
+			stuList.pTail->data.gpa.GPA_All += gpa * credit;
+			stuList.pTail->data.gpa.credits_General += credit;
+		}
+		if (check == 2) {
+			stuList.pTail->data.gpa.credits_Sub += credit;
+		}
+	}
+
+}
 
 int main() {
 	std::cout << "Test" << std::endl;
--- DSA_Challenges/Major Selection/struct.h
+++ DSA_Challenges/Major Selection/struct.h
@@ -1,1 +1,71 @@
-#pragma once
+#ifndef _STRUCT_H_
+#define _STRUCT_H_
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+#include <codecvt>
+#include <locale>
+
+//student
+struct Date {
+	int day, month, year;
+};
+
+struct MajorStu {
+	std::string desiredMajor[6];
+	int chosen = 0;
+};
+
+struct GPA {
+	float GPA_Foundation = 0;
+	float GPA_All = 0;
+	int credits_Foundation = 0;
+	int credits_General = 0;
+	int credits_Sub = 0;
+};
+
+struct Student {
+	std::string studentID;
+	std::string lastName;
+	std::string firstName;
+	Date regDate;
+	GPA gpa;
+	MajorStu interest;
+};
+
+struct _Student {
+	Student data;
+	_Student* pNextStu = nullptr;
+	_Student* pPreStu = nullptr;
+};
+
+struct StudentList {
+	_Student* pHead = nullptr;
+	_Student* pTail = nullptr;
+};
+//major
+
+struct Major {
+	std::wstring name;
+	std::string shortName;
+	int quota;
+
+	StudentList stuList;
+};
+
+struct _Major {
+	Major data;
+	_Major* pNextMajor = nullptr;
+	_Major* pPreMajor = nullptr;
+};
+
+
+//read file 
+void readGrading(std::string fileName, StudentList stuList);
+void readInterest(std::string fileName, StudentList stuList);
+void readMajor(std::string fileName, _Major* pHead);
+
+#endif _STRUCT_H_
